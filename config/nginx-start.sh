d7ca6973951a824273558b5522935888b68b8bb9
mouyigang <mouyigang@gmail.com>
2015-05-20 13:59:50 +0800

    Update nginx-start.sh

--- config/nginx-start.sh
+++ config/nginx-start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp /opt/etc/nginx.conf /etc/nginx/nginx.conf
 sed -i "s/%fpm-ip%/$FPM_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
+sed -i "s/%project_name%/$1/g" /etc/nginx/nginx.conf
 sed -i "s/%project_name%/$1/" /etc/nginx/sites-enabled/laravel
 exec /usr/sbin/nginx
